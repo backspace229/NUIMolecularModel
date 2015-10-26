0a54713801a924c0e61bbcb1a3d6903a9ac9de9a
backspace229 <backspace229@gmail.com>
2015-10-27 08:47:56 +0900

    出力プログラム追加(空)

---
+++ Assets/ExportXYZfromAtoms.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExportXYZfromAtoms : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("test");
+	}
+}
