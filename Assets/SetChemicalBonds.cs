90743829b0e389916d432571fa154d175ea3fe7f
backspace229 <backspace229@gmail.com>
2015-11-06 15:07:32 +0900

    化学結合用のSceneとScriptを追加

---
+++ Assets/SetChemicalBonds.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetChemicalBonds : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    // 60フレームごとに関数を呼び出す
+	}
+    // 原子の距離を判定して結合させるメソッド
+}
