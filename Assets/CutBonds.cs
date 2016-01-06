0648deaebb6e46b0eff91e3cb78e45eb449f41d5
backspace229 <backspace229@gmail.com>
2016-01-06 17:00:10 +0900

    結合をカットするスクリプトファイルを追加
    まだぜんぜん動かない状態

---
+++ Assets/CutBonds.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CutBonds : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        List<GameObject> BondsList = new List<GameObject>();
+        foreach (GameObject obj in UnityEngine.Object.FindObjectsOfType(typeof(GameObject)))
+        {
+            if (obj.tag == "ChemicalBond")
+                BondsList.Add(obj);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //OnTriggerEnter();
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(this);
+    }
+}
